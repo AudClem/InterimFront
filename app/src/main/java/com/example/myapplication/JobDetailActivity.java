40ceec52e0473fd5e8772bca6a45bca680dbc290
qsguily <qsguily@gmail.com>
2023-05-28 20:39:39 +0200

    added requests

--- app/src/main/java/com/example/myapplication/JobDetailActivity.java
+++ app/src/main/java/com/example/myapplication/JobDetailActivity.java
@@ -2,10 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class JobDetailActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
