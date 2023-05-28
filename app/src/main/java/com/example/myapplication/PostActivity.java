203828bd97ea494707fc48243f79fc8dca8e9338
qsguily <qsguily@gmail.com>
2023-05-28 15:56:40 +0200

    added post request

--- app/src/main/java/com/example/myapplication/PostActivity.java
+++ app/src/main/java/com/example/myapplication/PostActivity.java
@@ -6,27 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class PostActivity extends AppCompatActivity {
-
+    private String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_post);
 
+        Bundle extras = getIntent().getExtras();
+        if( extras != null ){
+            userId = extras.getString("userId");
+        }
+
         Button cta = findViewById( R.id.cta );
-
         cta.setOnClickListener( event -> {
-            String txtName = String.valueOf(((EditText)findViewById( R.id.txtName )).getText());
-            String txtJobTitle = String.valueOf(((EditText)findViewById( R.id.txtJobTitle )).getText());
-            String txtLocation = String.valueOf(((EditText)findViewById( R.id.txtLocation )).getText());
-            String txtStart = String.valueOf(((EditText)findViewById( R.id.txtStart )).getText());
-            String txtEnd = String.valueOf(((EditText)findViewById( R.id.txtEnd )).getText());
-            String txtRemuneration = String.valueOf(((EditText)findViewById( R.id.txtRemuneration )).getText());
-
+            Request.Response res = post();
+            if( res.getCode() == 200 ){
+                System.out.println( res.getString(0,"message") );
+            }
         });
     }
 
-
+    private Request.Response post(){
+        String url="http://10.0.2.2:5000/offer/create";
+        Request.Body data = new Request.Body();
+        data.put( "id_user", userId );
+        data.put( "name", String.valueOf(((EditText)findViewById( R.id.txtName )).getText()) );
+        data.put( "metier", String.valueOf(((EditText)findViewById( R.id.txtJobTitle )).getText()) );
+        data.put( "txtLocation", String.valueOf(((EditText)findViewById( R.id.txtLocation )).getText()) );
+        data.put( "dateDeb", String.valueOf(((EditText)findViewById( R.id.txtStart )).getText()) );
+        data.put( "dateFin", String.valueOf(((EditText)findViewById( R.id.txtEnd )).getText()) );
+        data.put( "remuneration", String.valueOf(((EditText)findViewById( R.id.txtRemuneration )).getText()) );
+        return Request.post( url, data );
+    }
 }
