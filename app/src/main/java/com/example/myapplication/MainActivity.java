ffa0149a397ec5baf79ef53d0747ea3e7a304a1e
qsguily <qsguily@gmail.com>
2023-05-28 19:13:01 +0200

    added back arrow

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,9 +16,7 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        Intent intent = new Intent (this, JobApplication.class);
-        intent.putExtra("userId", "b2cf324f-6b25-4074-8420-050441e73691");
-        intent.putExtra("offreId", "7aa99801-3ef8-4a51-9caf-786438f3fdbe");
+        Intent intent = new Intent (this, HomeActivity.class);
         startActivity(intent);
     }
 }
