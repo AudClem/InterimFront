1f6fc5b06104c0a11d0d7d2e690e2503bccd912b
qsguily <qsguily@gmail.com>
2023-05-28 18:16:28 +0200

    added job application request

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        Intent intent = new Intent (this, HomeActivity.class);
+        Intent intent = new Intent (this, JobApplication.class);
+        intent.putExtra("userId", "b2cf324f-6b25-4074-8420-050441e73691");
+        intent.putExtra("offreId", "7aa99801-3ef8-4a51-9caf-786438f3fdbe");
         startActivity(intent);
     }
 }
